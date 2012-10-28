164638bc8094a8024e858c022aa6b01d358a06e5
Shane Tomlinson <stomlinson@mozilla.com>
2012-10-28 17:52:59 +0000

    Start a crawlers directory, move the default crawler to crawlers/web-crawler.js

--- server/start.js
+++ server/start.js
@@ -3,12 +3,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const express    = require('express'),
-      path       = require('path'),
-      url        = require('url'),
-      get_page   = require('./lib/get-page'),
-      crawler    = require('./lib/crawler'),
-      pages      = require('./lib/pages');
+const express     = require('express'),
+      path        = require('path'),
+      url         = require('url'),
+      get_page    = require('./lib/get-page'),
+      web_crawler = require('./lib/crawlers/web-crawler.js'),
+      pages       = require('./lib/pages');
 
 function shouldIndex(parsedRoot, parsedLink) {
   try {
